6ad8990da797d508994e117ff6dde1f010c74cf0
NLefebvre7 <57395815+NLefebvre7@users.noreply.github.com>
2020-10-26 04:52:43 +0100

    bodyparser
    ajout body parser
    submit atterie ds terminal powershell

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const connectDb = require("./src/connection"); //connect mongodb
 const User = require("./src/user.model"); //prend model user depuis user.model.js
+const bodyParser = require('body-parser')
 const PORT = 8080;
 
-// localhost:8080
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-});
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
 
 //terminal powershell
 app.listen(PORT, function() {
@@ -19,18 +20,28 @@ connectDb().then(() => {
     console.log("MongoDb connected");
 });
 
+// localhost:8080/
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+// localhost:8080/quotes
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
 
 //test creation user sans utiliser post http verb : 
 
 //recupere les users et leurs infos
-app.get("/users", async(req, res) => {
-    const users = await User.find();
-    res.json(users);
-});
+// app.get("/users", async(req, res) => {
+//     const users = await User.find();
+//     res.json(users);
+// });
 
 // la page user-create creer l'user
-app.get("/user-create", async(req, res) => {
-    const user = new User({ prenom: 'prenom7', nom: 'nom7', age: 7 }); //dapres le model user en attendant de le faire avec index.html
-    await user.save().then(() => console.log("User created"));
-    res.send("User created \n");
-});
+// app.get("/user-create", async(req, res) => {
+//     const user = new User({ prenom: 'prenom7', nom: 'nom7', age: 7 }); //dapres le model user en attendant de le faire avec index.html
+//     await user.save().then(() => console.log("User created"));
+//     res.send("User created \n");
+// });
